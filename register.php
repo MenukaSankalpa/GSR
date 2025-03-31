ef0e6ddb438489224f4be50eba25ae8e8ba47704
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-31 15:05:19 +0530

    register.php done

--- register.php
+++ register.php
@@ -2,11 +2,20 @@
 include 'db.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST[];
-    $email = $_POST[];
-    $child_name = $_POST[];
-    $role = $_POST[];
-    $password =$_POST[];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $child_name = $_POST['child_name'];
+    $role = $_POST['role'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT );
+
+
+    $sql = "INSERT INTO users (username, email, child_name, role, password) VALUES ('$username', '$$child_name', '$role', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registation Successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
 
 ?>
