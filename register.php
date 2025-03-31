5312767c43752af078be1a37dde949a536981c05
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-31 14:04:05 +0530

    register.php update

---
+++ register.php
@@ -0,0 +1,12 @@
+<?php
+include 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST[];
+    $email = $_POST[];
+    $child_name = $_POST[];
+    $role = $_POST[];
+    $password =$_POST[];
+}
+
+?>
