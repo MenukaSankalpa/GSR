f6c390001ba1a3a0bf629fda307321c35d8ef82e
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-06 19:29:36 +0530

    forgot_password.php file added codes

---
+++ foregot_password.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot Password</title>
+</head>
+<body>
+    <h2>Forgot Password</h2>
+    <form action="" method="POST">
+        <input type="email" name="email" id="" placeholder="Enter your email" required>
+        <button type="submit">Send Reset Link</button>
+    </form>
+</body>
+</html>
