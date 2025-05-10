9b6ff8839cca38984ab86826c85c88ae8eb715cb
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-10 15:49:42 +0530

    reset_password.php updated

--- reset_password.php
+++ reset_password.php
@@ -1,31 +1,25 @@
 <?php
 include 'db.php';
 
-$token = $_GET['token'];
-$sql = "SELECT * FROM users WHERE reset_token = ? AND token_expiry > NOM()";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-$result = $stmt->get_result();
+if (isset($_GET['token'])) {
+    $token = $_GET['token'];
 
-if ($result->num_rows == 0) {
-    echo "Invalid or expired token.";
-    exit;
-}
-?>
-<!--Reset Password Form-->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reset Password</title>
-</head>
-<body>
-    <h2>Set New Password</h2>
-    <form action="update_password.php" method="POST">
-        <input type="hidden" name="token" value="<?php echo $token; ?>">
-        <input type="password" name="password" placeholder="New Password" required>
-        <button type="submit">Update Password</button>
-    </form>
-</body>
-</html>
+    //Validate  token and show reset form
+    $stmt = $conn->prepare("SELECT * FROM users WHERE reset_token = ? AND token_expiry > NOW()");
+    $stmt->bind_param("s", $token);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($result->num_rows ===1){
+        ?>
+            <form action="update_password.php" method="POST">
+                <input type="hidden" name="token" value="<?php echo $token; ?>">
+                <input type="password" name="password" placeholder="New Password" required>
+                <button type="submit">Update Password</button>
+            </form>
+        <?php
+    } else {
+        echo "Invalid or expired token.";
+    }
+}  
+?>  
