5c1b84a51318a229a44f4193df150d454d99da09
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-05-13 22:03:34 +0530

    added updated database file

--- school_admission_system.sql
+++ school_admission_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 01, 2025 at 02:53 PM
+-- Generation Time: May 13, 2025 at 06:32 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -30,9 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `schools` (
   `id` int(11) NOT NULL,
   `name` varchar(255) DEFAULT NULL,
-  `latitude` float DEFAULT NULL,
-  `longitude` float DEFAULT NULL,
-  `gender` enum('Boy','Girl','Mixed') DEFAULT NULL
+  `latitude` decimal(10,6) DEFAULT NULL,
+  `longitude` decimal(10,6) DEFAULT NULL,
+  `type` enum('boys','girls','mixed') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -49,16 +49,22 @@ CREATE TABLE `users` (
   `role` enum('1','2','3') DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `national_id` varchar(20) DEFAULT NULL,
-  `address` text DEFAULT NULL
+  `address` text DEFAULT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `token_expiry` datetime DEFAULT NULL,
+  `child_gender` varchar(10) DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `child_name`, `role`, `password`, `national_id`, `address`) VALUES
-(1, 'SuperAdmin', 'superadmin@gmail.com', 'N/A', '3', 'd1e576b71ccef5978d221fadf4f0e289', NULL, NULL),
-(7, 'Menuka Sankalpa', 'alone.monster13@gmail.com', 'Kavindu subasinha', '1', 'e10adc3949ba59abbe56e057f20f883e', NULL, NULL);
+INSERT INTO `users` (`id`, `username`, `email`, `child_name`, `role`, `password`, `national_id`, `address`, `reset_token`, `token_expiry`, `child_gender`, `latitude`, `longitude`) VALUES
+(1, 'SuperAdmin', 'superadmin@gmail.com', 'N/A', '3', 'd1e576b71ccef5978d221fadf4f0e289', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(9, 'Menuka Deshapriya', 'alone.monster13@gmail.com', 'Chamara Virasingha', '1', '25f9e794323b453885f5181f1b624d0b', '200310400766', '\"deshapriya\" samagi mawatha,duwa temple road , kalutara south ', '6f7652757aa41179b8aa80ed294185023765388a1776012112a6ba3f2b07481888bf9e622b0e1c0ad8e4e9ade2b6acc77bad', '2025-05-10 10:37:22', 'boy', 0, 0),
+(10, 'Kavidu Chethiya', 'test01@gmail.com', 'N/A', '2', '25f9e794323b453885f5181f1b624d0b', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -91,7 +97,7 @@ ALTER TABLE `schools`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
